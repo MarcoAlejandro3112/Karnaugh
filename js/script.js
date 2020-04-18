7184af9e3d9167701f1f07229ba1316f317e84a0
Marco Altamirano <maaltamiranohiga@gmail.com>
2020-04-18 17:03:20 -0500

    boolean tables

--- js/script.js
+++ js/script.js
@@ -12,10 +12,14 @@ function sePuede(arg){
 
 function a(arg){ 
 	if(arg == "="){
-			if(screen.value.indexOf("p") != -1){ mostrarF('p');}
-			if(screen.value.indexOf("q") != -1){ mostrarF('q');}
-			if(screen.value.indexOf("r") != -1){ mostrarF('r');}
-			if(screen.value.indexOf("s") != -1){ mostrarF('s');}
+		cleanScreen('p');
+		cleanScreen('q');
+		cleanScreen('r');
+		cleanScreen('s');
+		if(screen.value.indexOf("p") != -1){ mostrarF('p');}
+		if(screen.value.indexOf("q") != -1){ mostrarF('q');}
+		if(screen.value.indexOf("r") != -1){ mostrarF('r');}
+		if(screen.value.indexOf("s") != -1){ mostrarF('s');}
 	}
 	if(sePuede(arg) && arg != "=" && arg != "AC" && arg != "DEL"){
 		screen.value += arg;
@@ -35,14 +39,67 @@ function a(arg){
 
 }
 
+function cleanScreen(arg){
+	while(document.getElementById(arg)){
+		let ul = document.getElementById("ul" + arg.toUpperCase());
+		ul.removeChild(document.getElementById(arg));
+	}
+}
+
+function cuantasVar(){
+	let total = 0;
+	if(screen.value.indexOf("p") != -1){	
+		total++;
+	}
+	if(screen.value.indexOf("q") != -1){	
+		total++;
+	}
+	if(screen.value.indexOf("r") != -1){	
+		total++;
+	}
+	if(screen.value.indexOf("s") != -1){	
+		total++;
+	}
+	return total;
+}
+
 function mostrarF(arg){
 	const ul = (arg == 'p') ? document.getElementById("ulP") : (arg == 'q') 
 	? document.getElementById("ulQ") : (arg == 'r') ? document.getElementById("ulR") :
 	 document.getElementById("ulS");  
-		for(i = 0;i<8;i++){
+		for(i = 0;i<Math.pow(2,cuantasVar());i++){
 			let li = document.createElement("li");
+			li.id = arg;
+			if(arg == 'p'){
+				if(i < (cuantasVar() * 4)/2){
+					li.appendChild(document.createTextNode("V"));
+				} else {
+					li.appendChild(document.createTextNode("F"));
+				}
+			}
+			if(arg == 'q'){
+				if((i < (cuantasVar() * 4)/(cuantasVar()) || (i < (cuantasVar() * 4) 
+				- ((cuantasVar() * 4)/cuantasVar()) && i > ((cuantasVar()*4)/2)-1))){
+				li.appendChild(document.createTextNode("V"));
+				} else {
+				li.appendChild(document.createTextNode("F"));
+				}
+			}
+			if(arg == 'r'){
+				if(parseInt(i/2) %2 == 0){
+				li.appendChild(document.createTextNode("V"));
+				} else {
+				li.appendChild(document.createTextNode("F"));
+				}
+			}
+			if(arg == 's'){
+				if(i % 2 == 0){
+				li.appendChild(document.createTextNode("V"));
+				} else {
+				li.appendChild(document.createTextNode("F"));
+				}
+			}
 			ul.appendChild(li);
-			li.appendChild(document.createTextNode("V"));
 		}
 }
 
